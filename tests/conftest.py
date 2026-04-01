9015d29cd5c3d4c4eb78e384f07be883be8641fa
Subash Mahato <subashdhakal002@gmail.com>
2026-04-01 16:12:07 +0545

    Fix CI to use Chrome on Ubuntu runner

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,34 @@
 import pytest
+import os
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
-    options = webdriver.EdgeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Edge(
-        service=Service("drivers/msedgedriver.exe"),
-        options=options
-    )
+    browser = os.environ.get("BROWSER", "edge")
+    
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=options
+        )
+    else:
+        options = webdriver.EdgeOptions()
+        options.add_argument("--start-maximized")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Edge(
+            service=EdgeService("drivers/msedgedriver.exe"),
+            options=options
+        )
+    
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+
